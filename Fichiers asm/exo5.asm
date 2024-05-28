274864d7ed339fa5999dd00b9dd3e711f85a688f
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2024-05-28 09:44:27 +0200

    Rangement + des fichiers & exo  fctnel

--- Fichiers asm/exo5.asm
+++ Fichiers asm/exo5.asm
@@ -1,22 +1,29 @@
-; Programme exo 5
+.DATA
+	chaine DW "bonjour."
 
-.DATA 
-		; Initialisations des variables
-		chaine DW "test."
-		arret DW "."
-		
 .CODE 
-		LEA SP,STACK
-		LEA R0,chaine
-		LD R1,%01110000
-		LD R0, 0 ; Effacer l’écran
-		OUT R0,5
-
-boucle: CMP [R0],arret
-		BEQ fin
-		ADD R1,[R0]
-		OUT [R0],5
-		JMP boucle
-fin: 	HLT
-		
-.STACK 4
+	LEA SP,STACK 
+	LD R0,%00000000  
+	LEA R1,chaine ; Adresse de la chaine -> R1
+	LD R2,%11110111
+	LD R3,10
+	LD R4,10
+	OUT R0,5
+boucle: CMP [R1],'.' ; Vérification de fin de chaîne
+	BEQ fin
+	OUT R4,1 ; Changer les coordonnées
+	OUT R3,2
+	OUT [R1],3 ; Envoyer le caractère courant
+	OUT R2,5
+	INC R1
+	ADD R4,10
+	JMP boucle
+fin: LD R2,%11110010 ; Soulignage
+	OUT R3,1
+	OUT R3,2
+	OUT R4,3
+	OUT R3,4
+	OUT R2,5
+	
+	HLT
+.STACK 10
